a43a8769c138e52510f96d92841c24670112320c
Kenttsai1 <kent122277@gmail.com>
2021-10-04 14:38:23 +0800

    hw2

---
+++ exercise/02HW/oak99html.js
@@ -0,0 +1,33 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+function table99(){
+    let table = ["<table><tr><th></th><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th></tr>"]
+    for (let i=1; i<=9; i++) {
+      let row = `<tr><th>${i}</th>`
+      for (let j=1; j<=9; j++) {
+        row+= `<td>${i*j}</td>`
+      }
+      row += "</tr>"
+      table.push(row)
+    }
+    return table.join('\n')+"</table>"
+}
+
+
+const app = new Application();
+
+app.use((ctx) =>
+{
+    ctx.response.body = 
+    `
+    <html>
+    
+    <body>
+    ${table99()}
+    </body>
+    </html>
+    `
+});
+console.log('start at : http://127.0.0.1:8000')
+await app.listen({ port: 8000 });
+
